7dfa6e16913f050680b7306dd1611622d724ae22
Cat Core <arthur.koalas@free.fr>
2020-08-03 12:36:38 +0200

    Setup Project

---
+++ src/main/java/fr/catcore/translated/server/TranslatedServer.java
@@ -0,0 +1,13 @@
+package fr.catcore.translated.server;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.SERVER)
+public class TranslatedServer implements DedicatedServerModInitializer {
+    @Override
+    public void onInitializeServer() {
+
+    }
+}
---
+++ src/main/java/fr/catcore/translated/server/TranslatedServerPreLaunch.java
@@ -0,0 +1,10 @@
+package fr.catcore.translated.server;
+
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+
+public class TranslatedServerPreLaunch implements PreLaunchEntrypoint {
+    @Override
+    public void onPreLaunch() {
+
+    }
+}
