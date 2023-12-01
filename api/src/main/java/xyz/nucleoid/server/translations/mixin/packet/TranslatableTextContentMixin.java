662d803a1550c402f64ca09e03ef7869cc515384
Patbox <patbox4@outlook.com>
2023-12-01 13:01:44 +0100

    Update to 1.20.3-rc1

--- api/src/main/java/xyz/nucleoid/server/translations/mixin/packet/TranslatableTextContentMixin.java
+++ api/src/main/java/xyz/nucleoid/server/translations/mixin/packet/TranslatableTextContentMixin.java
@@ -1,5 +1,6 @@
 package xyz.nucleoid.server.translations.mixin.packet;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.mojang.datafixers.kinds.App;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
@@ -14,14 +15,14 @@ import java.util.function.Function;
 @Mixin(TranslatableTextContent.class)
 public abstract class TranslatableTextContentMixin {
 
-    @Redirect(
+    @ModifyExpressionValue(
         method = "<clinit>",
         at = @At(value = "INVOKE",
             target = "Lcom/mojang/serialization/codecs/RecordCodecBuilder;mapCodec(Ljava/util/function/Function;)Lcom/mojang/serialization/MapCodec;"
         )
     )
-    private static MapCodec<TranslatableTextContent> stapi$addTranslationFallback(Function<RecordCodecBuilder.Instance<TranslatableTextContent>, ? extends App<RecordCodecBuilder.Mu<TranslatableTextContent>, TranslatableTextContent>> builder) {
-        return RecordCodecBuilder.mapCodec(builder).xmap(Function.identity(), (content) -> {
+    private static MapCodec<TranslatableTextContent> stapi$addTranslationFallback(MapCodec<TranslatableTextContent> original) {
+        return original.xmap(Function.identity(), (content) -> {
             if (content.getFallback() == null) {
                 var target = LocalizationTarget.forPacket();
                 if (target != null) {
