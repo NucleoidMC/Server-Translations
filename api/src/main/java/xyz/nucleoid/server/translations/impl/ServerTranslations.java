8e56186d8dfceb1582ba34a41e234969dc7134b5
Drex <nicknamedrex@gmail.com>
2024-06-02 00:19:19 +0200

    Update to 1.21-pre2

--- api/src/main/java/xyz/nucleoid/server/translations/impl/ServerTranslations.java
+++ api/src/main/java/xyz/nucleoid/server/translations/impl/ServerTranslations.java
@@ -193,7 +193,7 @@ public final class ServerTranslations implements IdentifiableResourceReloadListe
 
     @Override
     public Identifier getFabricId() {
-        return Identifier.method_60655(ID, "translations");
+        return Identifier.of(ID, "translations");
     }
 
     @Override
