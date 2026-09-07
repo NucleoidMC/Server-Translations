b5de4c7c516adc695f772105abba17140007b6ff
Patbox <39821509+Patbox@users.noreply.github.com>
2026-09-07 21:01:49 +0200

    Update to 26.3

--- api/src/main/java/xyz/nucleoid/server/translations/mixin/packet/BlockEntityInfoMixin.java
+++ api/src/main/java/xyz/nucleoid/server/translations/mixin/packet/BlockEntityInfoMixin.java
@@ -1,8 +1,12 @@
 package xyz.nucleoid.server.translations.mixin.packet;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.fabricmc.fabric.api.networking.v1.context.PacketContext;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.network.protocol.game.ClientboundLevelChunkPacketData;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import org.spongepowered.asm.mixin.Final;
@@ -13,22 +17,27 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 import xyz.nucleoid.server.translations.api.LocalizationTarget;
 import xyz.nucleoid.server.translations.impl.nbt.SignNbtLocalizer;
 
-@Mixin(targets = "net/minecraft/network/protocol/game/ClientboundLevelChunkPacketData$BlockEntityInfo")
-public class BlockEntityInfoMixin {
-    @Shadow @Final private BlockEntityType<?> type;
+import java.util.Optional;
+import java.util.function.Function;
 
-    @ModifyArg(method = "write", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/RegistryFriendlyByteBuf;writeNbt(Lnet/minecraft/nbt/Tag;)Lnet/minecraft/network/FriendlyByteBuf;"))
-    private Tag stapi$translateNbt(Tag nbt) {
-        if (SignNbtLocalizer.isSign(this.type) && nbt instanceof CompoundTag compound) {
-            PacketContext context = PacketContext.get();
-            if (context != null) {
-                var target = LocalizationTarget.forPacket();
-                MinecraftServer server = context.get(PacketContext.SERVER_INSTANCE);
-                if (target != null && server != null) {
-                    return SignNbtLocalizer.translateNbt(compound, target, server.registryAccess());
+@Mixin(ClientboundLevelChunkPacketData.BlockEntityInfo.class)
+public class BlockEntityInfoMixin {
+
+    @ModifyExpressionValue(method = "<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/codec/StreamCodec;composite(Lnet/minecraft/network/codec/StreamCodec;Ljava/util/function/Function;Lnet/minecraft/network/codec/StreamCodec;Ljava/util/function/Function;Lnet/minecraft/network/codec/StreamCodec;Ljava/util/function/Function;Lnet/minecraft/network/codec/StreamCodec;Ljava/util/function/Function;Lcom/mojang/datafixers/util/Function4;)Lnet/minecraft/network/codec/StreamCodec;"))
+    private static StreamCodec<RegistryFriendlyByteBuf, ClientboundLevelChunkPacketData.BlockEntityInfo> patchCodec(StreamCodec<RegistryFriendlyByteBuf, ClientboundLevelChunkPacketData.BlockEntityInfo> original) {
+        return original.map(Function.identity(), info -> {
+            if (SignNbtLocalizer.isSign(info.type()) && info.tag().isPresent()) {
+                PacketContext context = PacketContext.get();
+                if (context != null) {
+                    var target = LocalizationTarget.forPacket();
+                    MinecraftServer server = context.get(PacketContext.SERVER_INSTANCE);
+                    if (target != null && server != null) {
+                        return new ClientboundLevelChunkPacketData.BlockEntityInfo(info.packedXZ(), info.y(), info.type(),
+                                Optional.of(SignNbtLocalizer.translateNbt(info.tag().orElseThrow(), target, server.registryAccess())));
+                    }
                 }
             }
-        }
-        return nbt;
+            return info;
+        });
     }
 }
--- src/main/java/fr/catcore/server/translations/mixin/RecipeManagerMixin.java
+++ src/main/java/fr/catcore/server/translations/mixin/RecipeManagerMixin.java
@@ -9,8 +9,8 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 @Mixin(RecipeManager.class)
 public class RecipeManagerMixin {
 
-    @ModifyArg(method = "apply(Lnet/minecraft/world/item/crafting/RecipeMap;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;info(Ljava/lang/String;Ljava/lang/Object;)V", ordinal = 0, remap = false), index = 0)
+    /*@ModifyArg(method = "apply(Lnet/minecraft/world/item/crafting/RecipeMap;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;info(Ljava/lang/String;Ljava/lang/Object;)V", ordinal = 0, remap = false), index = 0)
     private String translated_loaded(String string, Object p0) {
         return Component.translatable("text.translated_server.loaded.recipe", p0).getString();
-    }
+    }*/
 }
